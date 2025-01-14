854b282f91211bc4f5c6c950e52046d89953af20
Turbobot <turbobot@vercel.com>
2025-01-14 17:36:34 +0530

    feat(create-turbo): apply official-starter transform

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
+"use client";
 
-interface button {
-    text: string
+import { ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode;
+  className?: string;
+  appName: string;
 }
 
-function Button({ text }: button) {
-    return (
-        <button>{text}</button>
-    )
-}
-
-export default Button
+export const Button = ({ children, className, appName }: ButtonProps) => {
+  return (
+    <button
+      className={className}
+      onClick={() => alert(`Hello from your ${appName} app!`)}
+    >
+      {children}
+    </button>
+  );
+};
