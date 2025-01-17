9d3c00692fefe873f667302d4ada2976e47441cb
yshak <Vyshakn29@gmail.com>
2025-01-17 18:43:18 +0530

    basic web connection done

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -16,15 +16,21 @@ let allSockets: rooms[] = [];
 wss.on('connection', function connection(socket, Request) {
     socket.on('error', console.error);
     const roomcode = Request.headers["roomcode"];
-    socket.send("you are connected");
+    if (!roomcode) {
+        socket.send("your roomid does not exist");
+        wss.close();
+        return
+    };
+
     if (typeof roomcode === "string") {
         allSockets.push({ roomId: roomcode, socket: socket });
     }
 
     socket.on('message', function message(data) {
-        const parsedData = JSON.parse(data.toString())
         allSockets.map((item) => {
-            item.socket.send(data.toString())
+            if (item.roomId === roomcode) {
+                item.socket.send(data.toString())
+            }
         })
     });
 
