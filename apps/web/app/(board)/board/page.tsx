989db972647bbe24a589b5107fb754111b9f120e
yshak <Vyshakn29@gmail.com>
2025-01-19 21:22:52 +0530

    removed fabric starting with scratch

---
+++ apps/web/app/(board)/board/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from 'react'
+import { useRef, useEffect } from 'react'
+
+function page() {
+    const canvasRef = useRef()
+    return (
+        <div>
+            this is board
+        </div>
+    )
+}
+
+export default page
