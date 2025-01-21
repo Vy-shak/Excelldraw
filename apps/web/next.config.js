e2b072e1fa3046dd539e9d302eb5ee999dd18116
yshak <Vyshakn29@gmail.com>
2025-01-21 19:19:04 +0530

    add stuf

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,5 @@
-// /** @type {import('next').NextConfig} */
-export default {
+/** @type {import('next').NextConfig} */
+module.exports = {
     reactStrictMode: true,
     transpilePackages: [
         "@repo/ui",
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,9 +1,11 @@
-import type { Config } from "tailwindcss"
-import sharedConfig from "@repo-tailwind/tailwind-config"
+// tailwind config is required for editor support
+
+import type { Config } from "tailwindcss";
+import sharedConfig from "@repo/tailwind-config";
 
 const config: Pick<Config, "content" | "presets"> = {
     content: ["./app/**/*.tsx"],
     presets: [sharedConfig],
-}
+};
 
-export default config
+export default config;
