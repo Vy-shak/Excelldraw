41b0c176db4f6a4fd8c818b859921b82f3d9b344
yshak <Vyshakn29@gmail.com>
2025-01-22 12:04:12 +0530

    tailwind added fully

---
+++ apps/web/app/auth/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from 'react'
+import Button from "@repo/ui/Button"
+
+
+function page() {
+    return (
+        <div>
+            <Button size='sm' text='hello' variant='primary' />
+        </div>
+    )
+}
+
+export default page
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,44 @@
-"use client";
+"use client"
+interface buttonProps {
+  variant: 'primary' | 'secondary' | 'tertiary';
+  size: 'sm' | 'md' | 'lg' | 'full';
+  defaultCss?: string,
+  text: string;
+  startIcon?: any;
+  endIcon?: any;
+  handleClick?: () => void;
+};
 
-import { ReactNode } from "react";
 
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+const constants = {
+  variant: {
+    primary: 'bg-blue-500  font-normal text-white hover:bg-blue-500',
+    secondary: 'border-2 border-Breen-700  text-white opacity-80 font-Montserrat',
+    tertiary: 'bg-gray-50 w-fit focus:outline-0 outline outline-blue-100 outline-1 text-gray-900 text-sm '
+  },
+  size: {
+    sm: 'px-2 py-1 text-sm w-fit rounded-sm ',
+    md: 'px-6 font-Kanit py-2 text-base w-fit rounded-sm',
+    lg: 'px-6 py-3 text-lg w-fit',
+    full: 'text-sm w-full h-10'
+  },
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+
+
+const Button = (props: buttonProps) => {
+
   return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
-  );
-};
+    <>
+      <button onClick={props.handleClick} className={` bg-blue-500  ${props.defaultCss} ${constants.variant[props.variant]} ${constants.size[props.size]}`}>
+        <div className="flex w-fit  h-fit justify-center items-center leading-3">
+          {props.startIcon}
+          <span className="text-xs">{props.text}</span>
+          {props.endIcon}
+        </div>
+      </button>
+    </>
+  )
+}
+
+export default Button
