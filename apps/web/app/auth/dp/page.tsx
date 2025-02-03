3fcd1e589d218a878824a98c84e64aa71a0f72bd
yshak <Vyshakn29@gmail.com>
2025-02-04 02:03:01 +0530

    added key for avatars

--- apps/web/app/auth/dp/page.tsx
+++ apps/web/app/auth/dp/page.tsx
@@ -4,20 +4,21 @@ import { Avatar1, Avatar2, Avatar3, Avatar4, Avatar5, Avatar6, Avatar7, Avatar8,
 
 
 const avatarData = [
-    { avatarImg: Avatar2 },
-    { avatarImg: Avatar3 },
-    { avatarImg: Avatar4 },
-    { avatarImg: Avatar5 },
-    { avatarImg: Avatar6 },
-    { avatarImg: Avatar7 },
-    { avatarImg: Avatar8 },
-    { avatarImg: Avatar9 },
-    { avatarImg: Avatar10 },
-    { avatarImg: Avatar11 },
-    { avatarImg: Avatar12 },
-    { avatarImg: Avatar13 },
-    { avatarImg: Avatar14 }
+    { id: 1, avatarImg: Avatar2 },
+    { id: 2, avatarImg: Avatar3 },
+    { id: 3, avatarImg: Avatar4 },
+    { id: 4, avatarImg: Avatar5 },
+    { id: 5, avatarImg: Avatar6 },
+    { id: 6, avatarImg: Avatar7 },
+    { id: 7, avatarImg: Avatar8 },
+    { id: 8, avatarImg: Avatar9 },
+    { id: 9, avatarImg: Avatar10 },
+    { id: 10, avatarImg: Avatar11 },
+    { id: 11, avatarImg: Avatar12 },
+    { id: 12, avatarImg: Avatar13 },
+    { id: 13, avatarImg: Avatar14 }
 ];
+
 const page = () => {
     return (
         <section className='w-screen flex justify-start pt-16 items-start h-screen bg-white'>
@@ -29,7 +30,7 @@ const page = () => {
                     <span className='text-xs font-semibold text-neutral-700'>Avatars</span>
                     <div className='flex flex-wrap max-w-lg justify-start gap-2 items-center w-full'>
                         {avatarData.map((item) => (
-                            <Image src={item.avatarImg} alt='avtarimg' />
+                            <Image key={item.id} src={item.avatarImg} alt='avtarimg' />
                         ))}
                     </div>
                 </div>
