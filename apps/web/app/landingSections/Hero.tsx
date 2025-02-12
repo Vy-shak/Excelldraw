0b4c21a15d912f1e6a0c341ecd9bf1de627ff424
yshak <Vyshakn29@gmail.com>
2025-02-12 21:42:59 +0530

    hero section

--- apps/web/app/landingSections/Hero.tsx
+++ apps/web/app/landingSections/Hero.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import { TickIcon } from '../../public'
+import Image from 'next/image';
+import { Button } from '../../components';
+
+const Types = [
+    { id: 1, text: "Startups" },
+    { id: 2, text: "Students" },
+    { id: 3, text: "Teams" }
+];
 
 function Hero() {
     return (
-        <section className='w-full flexCenter'>
-            <h6 className='font-extrabold text-neutral-950'><span>Transform ideas,</span><br />Defy boundaries.</h6>
+        <section className='w-full flexCenter flex-col gap-y-6'>
+            <div className='w-full flexCenter flex-col gap-y-3'>
+                <h6 className='font-extrabold text-xl text-neutral-950'><span>Transform ideas,</span><br />Defy boundaries.</h6>
+                <div className='flexCenter w-full gap-x-4 h-full'>
+                    {Types.map((item) => (
+                        <div className='flexCenter flex-col gap-x-1 w-fit h-fit'>
+                            <Image alt='tick' src={TickIcon} />
+                            <span className='text-neutral-950 text-[14px] font-semibold'>{item.text}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className='w-full flexCenter gap-x-2'>
+                <Button text='Create account' size='default' variant='primary' />
+                <Button text='Be a guest' size='default' variant='secondary' />
+            </div>
         </section>
     )
 }
